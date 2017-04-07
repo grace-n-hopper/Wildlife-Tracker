d2e7f9ba907d59504164efafb519b5e37d2224e9
Grace Stuart <gstuart@gmail.com>
2017-04-07 13:59:36 -0700

    add passing test for instantiation

---
+++ src/test/java/RangerTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class RangerTest {
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Ranger_instantiatesCorrectly_true() {
+    Ranger ranger = new Ranger ("Wild Bill");
+    assertEquals(true, ranger instanceof Ranger);
+  }
+
+}
