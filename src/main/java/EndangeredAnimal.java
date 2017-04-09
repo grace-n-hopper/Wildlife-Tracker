af6d2d2185b955038e8054bba0027919e0de467b
Grace Stuart <gstuart@gmail.com>
2017-04-09 13:19:28 -0700

    add list sightings method

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -86,4 +86,14 @@ public class EndangeredAnimal {
         .executeUpdate();
     }
   }
+
+  public List<Sighting> getSightings() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sightings WHERE animal_id = :id;";
+        List<Sighting> sightings = con.createQuery(sql)
+          .addParameter("id", id)
+          .executeAndFetch(Sighting.class);
+      return sightings;
+    }
+  }
 }
