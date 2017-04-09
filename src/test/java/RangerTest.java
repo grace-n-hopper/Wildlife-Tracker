bd4cf48083c94ca6ff1a2ae1e032e0766d1f14c1
Grace Stuart <gstuart@gmail.com>
2017-04-09 12:50:34 -0700

    remove quotes around badge to match change from string to int

--- src/test/java/RangerTest.java
+++ src/test/java/RangerTest.java
@@ -11,78 +11,76 @@ public class RangerTest {
 
   @Test
   public void Ranger_rangerInstantiatesCorrectly_true() {
-    Ranger ranger = new Ranger ("Wild Bill", "1234");
+    Ranger ranger = new Ranger ("Wild Bill", 1234);
     assertEquals(true, ranger instanceof Ranger);
   }
 
   @Test
   public void getId_rangerInstantiatesWithId_int() {
-    Ranger newRanger = new Ranger("Wild Bill", "1234");
+    Ranger newRanger = new Ranger("Wild Bill", 1234);
     newRanger.save();
     assertTrue(newRanger.getId() > 0);
   }
 
   @Test
   public void getName_RangerInstantiatesWithName_string() {
-    Ranger ranger = new Ranger("Ray", "1234");
+    Ranger ranger = new Ranger("Ray", 1234);
     ranger.save();
     assertEquals("Ray", ranger.getName());
   }
 
   @Test
-  public void getBadge_rangerInstantiatesWithBadge_string(){
-    Ranger ranger = new Ranger("Ray", "1234");
+  public void getBadge_rangerInstantiatesWithBadge_string() {
+    Ranger ranger = new Ranger("Ray", 1234);
     ranger.save();
-    assertEquals("1234", ranger.getBadge());
+    assertEquals(1234, ranger.getBadge());
   }
 
   @Test
   public void equals_retunsTrueIfNameEnteredIntoDBAreReturnedCorrectly_true() {
-    Ranger ranger = new Ranger("Ray", "1234");
-    Ranger newRanger = new Ranger("Ray", "1234");
+    Ranger ranger = new Ranger("Ray", 1234);
+    Ranger newRanger = new Ranger("Ray", 1234);
     assertTrue(ranger.equals(newRanger));
   }
 
   @Test
   public void save_savesRangerIntoDatabase_true() {
-    Ranger ranger = new Ranger("Ray", "1234");
+    Ranger ranger = new Ranger("Ray", 1234);
     ranger.save();
     assertTrue(Ranger.all().get(0).equals(ranger));
   }
 
-  // @Test
-  // public void all_returnsAllRangersFromDatabase_true() {
-  //   Ranger ranger = new Ranger("Ray", "1234");
-  //   ranger.save();
-  //   Ranger newRanger = new Ranger("Raymond", "1235");
-  //   newRanger.save();
-  //   // assertTrue(Ranger.all().get(0).equals(ranger));
-  //   assertTrue(Ranger.all().get(1).equals(newRanger));
-  //   // assertEquals(true, Ranger.all().get(0).equals(ranger));
-  //   // assertEquals(true, Ranger.all().get(1).equals(newRanger));
-  // }
+  @Test
+  public void all_returnsAllRangersFromDatabase_true() {
+    Ranger ranger = new Ranger("Ray", 1234);
+    ranger.save();
+    Ranger newRanger = new Ranger("Raymond", 1235);
+    newRanger.save();
+    assertTrue(Ranger.all().get(0).equals(ranger));
+    assertTrue(Ranger.all().get(1).equals(newRanger));
+  }
 
   @Test
   public void find_returnsSpecificRangerFromDatabase_true() {
-    Ranger ranger = new Ranger("Ray", "1234");
+    Ranger ranger = new Ranger("Ray", 1234);
     ranger.save();
-    Ranger newRanger = new Ranger("Raymond", "1235");
+    Ranger newRanger = new Ranger("Raymond", 1235);
     newRanger.save();
     assertEquals(ranger, Ranger.find(ranger.getId()));
   }
 
   @Test
-  public void update_updatesRangerNameAndOrBadgeInDatabase_string() {
-    Ranger ranger = new Ranger("Ray", "1234");
+  public void update_updatesRangerNameAndBadgeInDatabase() {
+    Ranger ranger = new Ranger("Ray", 1234);
     ranger.save();
-    ranger.update("Rae", "1235");
+    ranger.update("Rae", 1235);
     assertTrue(Ranger.find(ranger.getId()).getName().equals("Rae"));
-    assertEquals("1235", Ranger.find(ranger.getId()).getBadge());
+    assertTrue(Ranger.find(ranger.getId()).getBadge() == 1235);
   }
 
   @Test
   public void delete_deletestRanger_null() {
-    Ranger ranger = new Ranger("Ray", "1234");
+    Ranger ranger = new Ranger("Ray", 1234);
     ranger.save();
     ranger.delete();
     assertEquals(null, Ranger.find(ranger.getId()));
