3ebe1a5afa14128924f0db887a3d715b653de7b0
Grace Stuart <gstuart@gmail.com>
2017-04-09 18:29:26 -0700

    add imports for timestamp, add date to class properties, getDate method, and date stuff to save method

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -2,18 +2,22 @@ import org.sql2o.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.text.DateFormat;
+import java.sql.Timestamp;
 
 public class Sighting implements BasicInterface {
   private int animal_id;
   private String location;
   private String ranger_name;
   private int id;
+  private Timestamp date;
 
   public Sighting(int animal_id, String location, String ranger_name) {
     this.animal_id = animal_id;
     this.location = location;
     this.ranger_name = ranger_name;
     this.id = id;
+    this.date = date;
   }
 
   public int getId() {
@@ -32,6 +36,10 @@ public class Sighting implements BasicInterface {
     return ranger_name;
   }
 
+  public Timestamp getDate() {
+    return date;
+  }
+
   @Override
   public boolean equals(Object otherSighting) {
     if(!(otherSighting instanceof Sighting)) {
@@ -45,7 +53,7 @@ public class Sighting implements BasicInterface {
   @Override
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO sightings (animal_id, location, ranger_name) VALUES (:animal_id, :location, :ranger_name);";
+      String sql = "INSERT INTO sightings (animal_id, location, ranger_name, date) VALUES (:animal_id, :location, :ranger_name, now());";
       this.id = (int) con.createQuery(sql, true)
         .addParameter("animal_id", this.animal_id)
         .addParameter("location", this.location)
