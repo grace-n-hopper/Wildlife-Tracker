f88e6394b566433f182d8a43a5315baebaa4369c
Grace Stuart <gstuart@gmail.com>
2017-06-18 09:01:49 -0700

    correct credentials

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,13 +14,10 @@ public class App {
     }
     return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
   }
-  
+
   public static void main(String[] args) {
 
     port(getHerokuAssignedPort());
-    staticFileLocation("/public");
-    String layout = "templates/layout.vtl";
-  public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
