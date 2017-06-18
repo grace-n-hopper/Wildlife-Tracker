d20f58ae2ffe63f96e3bb43765cfecb4de4cf74c
Grace Stuart <gstuart@gmail.com>
2017-06-18 08:52:34 -0700

    add heroku db credentials

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,7 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", null, null);
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", null, null);
+
+  public static Sql2o sql2o = new Sql2o("postgres://ikxmphsvfomqwf:64002b121eb24d980423377eeb31f20e7a345e21618181d365e524ffb1aec359@ec2-184-72-246-219.compute-1.amazonaws.com:5432/d5kfstbtve8tv0";
 }
