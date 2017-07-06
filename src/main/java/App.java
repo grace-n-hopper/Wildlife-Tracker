f1bb5df1e3512a67ed41c2e50c80db0099bb82cd
Grace Stuart <gstuart@gmail.com>
2017-07-05 18:49:22 -0700

    add get route for animasl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -118,8 +118,8 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-//route when user clicks "All Animals"
-    get("/sighting", (request, response) -> {
+//route when user clicks "All Animals" or "View Animals"
+    get("/animals", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("animals", Animal.all());
       model.put("endangeredAnimals", EndangeredAnimal.all());
