1cd33f53fce5a3e825b4dcbbd5ec8b54160cb196
Grace Stuart <gstuart@gmail.com>
2017-07-05 21:32:36 -0700

    add database

--- wildlife_tracker.sql
+++ wildlife_tracker.sql
@@ -68,39 +68,6 @@ ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
 
 
 --
--- Name: conservation_statuses; Type: TABLE; Schema: public; Owner: Grace
---
-
-CREATE TABLE conservation_statuses (
-    id integer NOT NULL,
-    name character varying
-);
-
-
-ALTER TABLE conservation_statuses OWNER TO "Grace";
-
---
--- Name: conservation_statuses_id_seq; Type: SEQUENCE; Schema: public; Owner: Grace
---
-
-CREATE SEQUENCE conservation_statuses_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE conservation_statuses_id_seq OWNER TO "Grace";
-
---
--- Name: conservation_statuses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Grace
---
-
-ALTER SEQUENCE conservation_statuses_id_seq OWNED BY conservation_statuses.id;
-
-
---
 -- Name: endangered_animals; Type: TABLE; Schema: public; Owner: Grace
 --
 
@@ -133,107 +100,6 @@ ALTER TABLE endangered_animals_id_seq OWNER TO "Grace";
 --
 
 ALTER SEQUENCE endangered_animals_id_seq OWNED BY endangered_animals.id;
-
-
---
--- Name: locations; Type: TABLE; Schema: public; Owner: Grace
---
-
-CREATE TABLE locations (
-    id integer NOT NULL,
-    name character varying,
-    area character varying
-);
-
-
-ALTER TABLE locations OWNER TO "Grace";
-
---
--- Name: locations_id_seq; Type: SEQUENCE; Schema: public; Owner: Grace
---
-
-CREATE SEQUENCE locations_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE locations_id_seq OWNER TO "Grace";
-
---
--- Name: locations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Grace
---
-
-ALTER SEQUENCE locations_id_seq OWNED BY locations.id;
-
-
---
--- Name: rangers; Type: TABLE; Schema: public; Owner: Grace
---
-
-CREATE TABLE rangers (
-    id integer NOT NULL,
-    name character varying,
-    badge character varying
-);
-
-
-ALTER TABLE rangers OWNER TO "Grace";
-
---
--- Name: rangers_id_seq; Type: SEQUENCE; Schema: public; Owner: Grace
---
-
-CREATE SEQUENCE rangers_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE rangers_id_seq OWNER TO "Grace";
-
---
--- Name: rangers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Grace
---
-
-ALTER SEQUENCE rangers_id_seq OWNED BY rangers.id;
-
-
---
--- Name: scientific_classes; Type: TABLE; Schema: public; Owner: Grace
---
-
-CREATE TABLE scientific_classes (
-    id integer NOT NULL,
-    name character varying
-);
-
-
-ALTER TABLE scientific_classes OWNER TO "Grace";
-
---
--- Name: scientific_classes_id_seq; Type: SEQUENCE; Schema: public; Owner: Grace
---
-
-CREATE SEQUENCE scientific_classes_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE scientific_classes_id_seq OWNER TO "Grace";
-
---
--- Name: scientific_classes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Grace
---
-
-ALTER SEQUENCE scientific_classes_id_seq OWNED BY scientific_classes.id;
 
 
 --
@@ -273,50 +139,10 @@ ALTER SEQUENCE sightings_id_seq OWNED BY sightings.id;
 
 
 --
--- Name: stations; Type: TABLE; Schema: public; Owner: Grace
---
-
-CREATE TABLE stations (
-    id integer NOT NULL,
-    name character varying
-);
-
-
-ALTER TABLE stations OWNER TO "Grace";
-
---
--- Name: stations_id_seq; Type: SEQUENCE; Schema: public; Owner: Grace
---
-
-CREATE SEQUENCE stations_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE stations_id_seq OWNER TO "Grace";
-
---
--- Name: stations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Grace
---
-
-ALTER SEQUENCE stations_id_seq OWNED BY stations.id;
-
-
---
 -- Name: animals id; Type: DEFAULT; Schema: public; Owner: Grace
 --
 
 ALTER TABLE ONLY animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq'::regclass);
-
-
---
--- Name: conservation_statuses id; Type: DEFAULT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY conservation_statuses ALTER COLUMN id SET DEFAULT nextval('conservation_statuses_id_seq'::regclass);
 
 
 --
@@ -327,27 +153,6 @@ ALTER TABLE ONLY endangered_animals ALTER COLUMN id SET DEFAULT nextval('endange
 
 
 --
--- Name: locations id; Type: DEFAULT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY locations ALTER COLUMN id SET DEFAULT nextval('locations_id_seq'::regclass);
-
-
---
--- Name: rangers id; Type: DEFAULT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY rangers ALTER COLUMN id SET DEFAULT nextval('rangers_id_seq'::regclass);
-
-
---
--- Name: scientific_classes id; Type: DEFAULT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY scientific_classes ALTER COLUMN id SET DEFAULT nextval('scientific_classes_id_seq'::regclass);
-
-
---
 -- Name: sightings id; Type: DEFAULT; Schema: public; Owner: Grace
 --
 
@@ -355,18 +160,10 @@ ALTER TABLE ONLY sightings ALTER COLUMN id SET DEFAULT nextval('sightings_id_seq
 
 
 --
--- Name: stations id; Type: DEFAULT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY stations ALTER COLUMN id SET DEFAULT nextval('stations_id_seq'::regclass);
-
-
---
 -- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: Grace
 --
 
 COPY animals (id, name) FROM stdin;
-5	Deer
 \.
 
 
@@ -374,22 +171,7 @@ COPY animals (id, name) FROM stdin;
 -- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
 --
 
-SELECT pg_catalog.setval('animals_id_seq', 5, true);
-
-
---
--- Data for Name: conservation_statuses; Type: TABLE DATA; Schema: public; Owner: Grace
---
-
-COPY conservation_statuses (id, name) FROM stdin;
-\.
-
-
---
--- Name: conservation_statuses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
---
-
-SELECT pg_catalog.setval('conservation_statuses_id_seq', 1, false);
+SELECT pg_catalog.setval('animals_id_seq', 1, true);
 
 
 --
@@ -404,53 +186,7 @@ COPY endangered_animals (id, name, health, age) FROM stdin;
 -- Name: endangered_animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
 --
 
-SELECT pg_catalog.setval('endangered_animals_id_seq', 3, true);
-
-
---
--- Data for Name: locations; Type: TABLE DATA; Schema: public; Owner: Grace
---
-
-COPY locations (id, name, area) FROM stdin;
-\.
-
-
---
--- Name: locations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
---
-
-SELECT pg_catalog.setval('locations_id_seq', 1, false);
-
-
---
--- Data for Name: rangers; Type: TABLE DATA; Schema: public; Owner: Grace
---
-
-COPY rangers (id, name, badge) FROM stdin;
-1	Ray	\N
-\.
-
-
---
--- Name: rangers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
---
-
-SELECT pg_catalog.setval('rangers_id_seq', 1, true);
-
-
---
--- Data for Name: scientific_classes; Type: TABLE DATA; Schema: public; Owner: Grace
---
-
-COPY scientific_classes (id, name) FROM stdin;
-\.
-
-
---
--- Name: scientific_classes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
---
-
-SELECT pg_catalog.setval('scientific_classes_id_seq', 1, false);
+SELECT pg_catalog.setval('endangered_animals_id_seq', 1, true);
 
 
 --
@@ -465,22 +201,7 @@ COPY sightings (id, animal_id, location, ranger_name, date) FROM stdin;
 -- Name: sightings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
 --
 
-SELECT pg_catalog.setval('sightings_id_seq', 4, true);
-
-
---
--- Data for Name: stations; Type: TABLE DATA; Schema: public; Owner: Grace
---
-
-COPY stations (id, name) FROM stdin;
-\.
-
-
---
--- Name: stations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Grace
---
-
-SELECT pg_catalog.setval('stations_id_seq', 1, false);
+SELECT pg_catalog.setval('sightings_id_seq', 1, false);
 
 
 --
@@ -492,14 +213,6 @@ ALTER TABLE ONLY animals
 
 
 --
--- Name: conservation_statuses conservation_statuses_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY conservation_statuses
-    ADD CONSTRAINT conservation_statuses_pkey PRIMARY KEY (id);
-
-
---
 -- Name: endangered_animals endangered_animals_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
 --
 
@@ -508,43 +221,11 @@ ALTER TABLE ONLY endangered_animals
 
 
 --
--- Name: locations locations_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY locations
-    ADD CONSTRAINT locations_pkey PRIMARY KEY (id);
-
-
---
--- Name: rangers rangers_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY rangers
-    ADD CONSTRAINT rangers_pkey PRIMARY KEY (id);
-
-
---
--- Name: scientific_classes scientific_classes_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY scientific_classes
-    ADD CONSTRAINT scientific_classes_pkey PRIMARY KEY (id);
-
-
---
 -- Name: sightings sightings_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
 --
 
 ALTER TABLE ONLY sightings
     ADD CONSTRAINT sightings_pkey PRIMARY KEY (id);
-
-
---
--- Name: stations stations_pkey; Type: CONSTRAINT; Schema: public; Owner: Grace
---
-
-ALTER TABLE ONLY stations
-    ADD CONSTRAINT stations_pkey PRIMARY KEY (id);
 
 
 --
